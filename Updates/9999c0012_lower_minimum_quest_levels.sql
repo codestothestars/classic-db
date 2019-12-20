b3a9d6f257004b68921515c135750e13332cc8e4
Ariel Oak <codestothestars@yahoo.com>
2019-12-19 23:44:21 -0500

    Lower minimum level of Maraudon quests to 40
    Ensures that players can attempt that at the minimum recommended level
    for Maraudon.

--- Updates/9999c0012_lower_minimum_quest_levels.sql
+++ Updates/9999c0012_lower_minimum_quest_levels.sql
@@ -4,3 +4,5 @@
 UPDATE quest_template
 SET MinLevel = MinLevel - 1
 WHERE MinLevel > 1;
+
+UPDATE quest_template SET MinLevel = 40 WHERE entry IN (7028, 7029, 7041, 7044, 7046, 7064, 7065); -- Maraudon
